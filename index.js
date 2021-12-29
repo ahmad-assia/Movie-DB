4bcdfa70728ba47e68bc69890e69bf7efc5d7eb3
assia ahmad <ahmadassia676@gmail.com>
2021-12-29 15:31:48 +0200

    step3

--- index.js
+++ index.js
@@ -1,10 +1,22 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const port = 3001
 app.get('/', (req, res) => {
     res.send('ok')
  
 })
+
+
+app.get('/test', (req, res) => {
+    res.send( {status:200,message:"ok"})
+ 
+})
+app.get('/time', (req, res) => {
+var today = new Date();
+var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+ 
+})
+
 
 app.listen(port, () => console.log(`the server started at http://localhost:${port} `
 ))
