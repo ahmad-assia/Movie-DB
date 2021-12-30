6d9ba6c7469c7114f1765e00eb5477c249cb7007
assia ahmad <ahmadassia676@gmail.com>
2021-12-30 13:27:03 +0200

    step4

--- index.js
+++ index.js
@@ -17,6 +17,37 @@ var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds(
  
 })
 
-
+app.get("/hello/:id", (req, res) => {
+    res.send({
+      status: 200,
+      message: "hello",
+      Id: req.params.id,
+    });
+  });
+  app.get("/hello", (req, res) => {
+    res.send({
+      status: 200,
+      message: "hello",
+    });
+  });
+  
+  // Search
+  
+  app.get("/search/s=:search", (req, res) => {
+    res.send({
+      status: 200,
+      maeesage: "ok",
+      data: req.params.search,
+    });
+  });
+  
+  app.get("/search/s=", (req, res) => {
+    res.send({
+      status: 500,
+      error: true,
+      message: "you have to provide a search",
+    });
+  });
+  
 app.listen(port, () => console.log(`the server started at http://localhost:${port} `
 ))
